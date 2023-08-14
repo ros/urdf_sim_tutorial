63fbdc676aa67d4ead7b7e5dac283386ea297ed3
David V. Lu <davidvlu@gmail.com>
2023-08-14 12:10:58 -0400

    13 basic

---
+++ launch/13-diffdrive.launch.py
@@ -0,0 +1,73 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    package_arg = DeclareLaunchArgument('urdf_package',
+                                        description='The package where the robot description is located',
+                                        default_value='urdf_sim_tutorial')
+    model_arg = DeclareLaunchArgument('urdf_package_path',
+                                      description='The path to the robot description relative to the package root',
+                                      default_value='urdf/13-diffdrive.urdf.xacro')
+
+    rvizconfig_arg = DeclareLaunchArgument(
+        name="rvizconfig",
+        default_value=[FindPackageShare('urdf_tutorial'), '/rviz/urdf.rviz'],
+    )
+
+    gazebo_launch = IncludeLaunchDescription(
+        [FindPackageShare('urdf_sim_tutorial'), '/launch/gazebo.launch.py'],
+        launch_arguments={
+            'urdf_package': LaunchConfiguration('urdf_package'),
+            'urdf_package_path': LaunchConfiguration('urdf_package_path')
+        }.items(),
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+    load_head_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'head_controller'],
+        output='screen'
+    )
+    load_gripper_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'gripper_controller'],
+        output='screen'
+    )
+
+    load_dd_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'start',
+             'diff_drive_base_controller'],
+        output='screen'
+    )
+    rqt_robot_steering_node = Node(
+        package='rqt_robot_steering',
+        executable='rqt_robot_steering',
+    )
+    return LaunchDescription([
+        package_arg,
+        model_arg,
+        rvizconfig_arg,
+        gazebo_launch,
+        rviz_node,
+        load_joint_state_controller,
+        load_head_controller,
+        load_gripper_controller,
+        load_dd_controller,
+        rqt_robot_steering_node
+    ])
